ef3746e6b1ebde68fdb8df8dc1f1629d6b4cfd73
Adiladis <adam.lysen@elev.ga.ntig.se>
2023-08-31 10:52:54 +0200

    rgerferf

---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+
+Console.WriteLine("Vad heter do??");
+string name = Console.ReadLine();
+
+Console.WriteLine("varfor du ticker om cheeseburgar?");
+string cheeseburgar = Console.ReadLine();
+
+Console.WriteLine("Kan du göra en gång till och sen aldrig igen?");
+string fråga = Console.ReadLine();
+
+Console.WriteLine($"OM {name} TICKER DET ÄR {cheeseburgar} DÅ HAN SKA DÖD OCH SEN HAN GÖRA IGEN {fråga}");
+
+
+Console.ReadLine();
